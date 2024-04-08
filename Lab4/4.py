b59e2ba9f85ba4c5cb70bde73fb2d14fdbca0341
Prithwiraj Mazumdar <165425986+Prithwiraj14@users.noreply.github.com>
2024-04-08 19:11:04 +0530

    Delete Lab4 directory

--- Lab4/4.py
+++
@@ -1,6 +0,0 @@
-s = set([12, 10, 13, 15, 8, 9])
-sorted_list = sorted(s, reverse=True)
-
-for i in range(len(sorted_list)):
-    print(set(sorted_list[i:]))
-print(list(s))
--- Lab4/5.py
+++
@@ -1,6 +0,0 @@
-def base_to_decimal(num, base):
-    return int(num, base)
-num = '1011'
-base = 2
-output = base_to_decimal(num, base)
-print(output)
